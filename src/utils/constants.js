3963aded6365ff993d63c4b3b118eb192fec9371
Lohit <kodathalalohith18@gmail.com>
2026-06-23 14:32:29 +0530

    Fix YouTube search suggestions API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY
 const YOUTUBE_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&key=${API_KEY}&q=`
-const YOUTUBE_SEARCH_API = `/api/suggestions?client=firefox&ds=yt&q=`
+const YOUTUBE_SEARCH_API = `https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`
 const VIDEOS_API = `https://www.googleapis.com/youtube/v3/videos?part=statistics&key=${API_KEY}&id=`
 const COMMENTS_API = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&maxResults=20&key=${API_KEY}&videoId=`
 
